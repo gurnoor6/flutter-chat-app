70e617174a96deeac29ee51b28a24c4a7d0d4760
Gurnoor Singh Khurana <gurnoor6@gmail.com>
2020-06-14 18:39:54 +0530

    Initial Commit

---
+++ lib/widgets/widget.dart
@@ -0,0 +1,36 @@
+import "package:flutter/material.dart";
+
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    title: Image.asset("assets/images/logo.png", height: 50),
+  );
+}
+
+InputDecoration textFieldInputDecoration(String hintText){
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(
+          color: Colors.white54
+      ),
+      focusedBorder: UnderlineInputBorder(
+        borderSide: BorderSide(color:Colors.white),
+      ),
+      enabledBorder:UnderlineInputBorder(
+        borderSide: BorderSide(color:Colors.white),
+      )
+  );
+}
+
+TextStyle simpleTextStyle(){
+  return TextStyle(
+    fontSize: 16,
+    color:Colors.white,
+  );
+}
+
+TextStyle mediumTextStyle(){
+  return TextStyle(
+    fontSize: 17,
+    color:Colors.white,
+  );
+}
